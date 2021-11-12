09337266a61a3a7f1bda03a69125b6148e50a4e6
KonishiNaoto <kd1258075@st.kobedenshi.ac.jp>
2021-11-12 14:33:58 +0900

    【小西】シーン遷移時のフェードインフェードアウト

--- Melody of BattleField/Assets/Script/Konisi/System/ChangeScene.cs
+++ Melody of BattleField/Assets/Script/Konisi/System/ChangeScene.cs
@@ -8,9 +8,23 @@ public class ChangeScene : MonoBehaviour
     // 遷移先のシーンの名前
     public string nextSceneName;
 
+    GameObject manageObject;
+
+    SceneFadeManager fadeManager;
+
+    private void Start()
+    {
+        //SceneFadeManagerがアタッチされているオブジェクトを取得
+        manageObject = GameObject.Find("FadeManageObject");
+        
+        //オブジェクトの中のSceneFadeManagerを取得
+        fadeManager = manageObject.GetComponent<SceneFadeManager>();
+    }
+
     // シーン遷移
     public void LoadNextScene()
     {
-        SceneManager.LoadScene(nextSceneName);
+        //SceneFadeManagerの中のフェードアウト開始関数を呼び出し
+        fadeManager.fadeOutStart(0, 0, 0, 0, nextSceneName);
     }
 }
