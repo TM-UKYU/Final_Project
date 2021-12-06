616bd394fce02a11b94b728de9663b3466adc857
KonishiNaoto <kd1258075@st.kobedenshi.ac.jp>
2021-12-06 12:18:02 +0900

    【小西】遊ぶステージの難易度と武器のstatic変数実装

---
+++ Melody of BattleField/Assets/Script/Konisi/Game/StageInformation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageInformation : MonoBehaviour
+{
+    public enum STAGE_ID
+    {
+        CRAB,
+        NORMAL,
+        HARD
+    };
+
+    public enum WEAPON_ID
+    {
+        KeyBoard
+    }
+
+    public static STAGE_ID Stage;
+    public static WEAPON_ID Weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
