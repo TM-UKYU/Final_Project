e30312049584d0292438744ab97c22b7f6589951
Matti <60887233+Matti@users.noreply.github.com>
2022-01-27 16:21:05 +0900

    [植松]プレイヤーのモデル変更

--- Melody of BattleField/Assets/Script/Uematu/PlayerScript/CreateDagger.cs
+++ Melody of BattleField/Assets/Script/Uematu/PlayerScript/CreateDagger.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CreateDagger : MonoBehaviour
 {
-    private GameObject daggerScript1;
-    private GameObject daggerScript2;
-    private GameObject daggerScript3;
+    public GameObject daggerScript1;
+    public GameObject daggerScript2;
+    public GameObject daggerScript3;
 
     public GameObject Player;
 
     public AudioClip audioClip;
+    public AudioClip audioClip2;
+    public AudioClip audioClip3;
 
     private SoundManager soundManager;
 
@@ -19,10 +21,13 @@ public class CreateDagger : MonoBehaviour
     public KeyCode AttackKey_2 = KeyCode.X;
     public KeyCode AttackKey_3 = KeyCode.Z;
 
+    private Animator animator;
+
     private void Start()
     {
         GameObject obj = GameObject.Find("SoundManager");
         soundManager = obj.GetComponent<SoundManager>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,6 +44,9 @@ public class CreateDagger : MonoBehaviour
 
         if (Input.GetKeyUp(AttackKey_1))
         {
+            Debug.Log("çUåÇ");
+            animator.SetTrigger("Attack");
+
             daggerScript1 = (GameObject)Resources.Load("Dagger1");
 
             //obj = Instantiate(daggerScript1, daggerScript1.transform.position, Quaternion.Euler(0, 90, 0));
@@ -52,17 +60,23 @@ public class CreateDagger : MonoBehaviour
         }
         else if (Input.GetKeyUp(AttackKey_2))
         {
+            animator.SetTrigger("Attack");
+
             daggerScript2 = (GameObject)Resources.Load("Dagger2");
 
             Instantiate(daggerScript2, DaggerPos.position + daggerScript2.transform.position,
                 Quaternion.Euler(0, (this.transform.rotation.y * 100) + 180, 0));
+            soundManager.SoundPlayOne(audioClip2);
         }
         else if (Input.GetKeyUp(AttackKey_3))
         {
+            animator.SetTrigger("Attack");
+
             daggerScript3 = (GameObject)Resources.Load("Dagger3");
 
             Instantiate(daggerScript3, DaggerPos.position + daggerScript3.transform.position,
                 Quaternion.Euler(0, (this.transform.rotation.y * 100) + 180, 0));
+            soundManager.SoundPlayOne(audioClip3);
         }
     }
 }
