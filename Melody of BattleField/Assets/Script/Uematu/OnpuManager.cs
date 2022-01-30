b47f7c6eeb8540ebceed02839685d5e140e3bdfe
Matti <60887233+Matti@users.noreply.github.com>
2022-01-31 00:01:50 +0900

    [植松]強攻撃無理やり実装

--- Melody of BattleField/Assets/Script/Uematu/OnpuManager.cs
+++ Melody of BattleField/Assets/Script/Uematu/OnpuManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OnpuManager : MonoBehaviour
 {
+    //前に何を押したか
     enum PreviousButton
     {
         Nome,
@@ -13,13 +15,19 @@ public class OnpuManager : MonoBehaviour
         Button3
     }
 
+    //キーコード
     public KeyCode Button1 = KeyCode.C;
     public KeyCode Button2 = KeyCode.V;
     public KeyCode Button3 = KeyCode.B;
 
+    public KeyCode EndButton = KeyCode.Escape;
+
+    //音符のイメージ
     public Image Onpu1;
     public Image Onpu2;
     public Image Onpu3;
+
+    public GameObject Dagger;
 
     private PreviousButton previousButton = PreviousButton.Nome;
 
@@ -56,11 +64,23 @@ public class OnpuManager : MonoBehaviour
             {
                 Onpu3.enabled = true;
                 previousButton = PreviousButton.Button3;
+
+                GameObject obj = GameObject.FindGameObjectWithTag("Player");
+
+                Instantiate(Dagger, obj.gameObject.transform.position + Dagger.transform.position,
+                Quaternion.Euler(0, (this.transform.rotation.y * 100) + 180, 0));
+
+                DestroyOnpu();
             }
             else
             {
                 DestroyOnpu();
             }
+        }
+
+        if (Input.GetKeyDown(EndButton))
+        {
+            SceneManager.LoadScene("TitleScene");
         }
     }
 
